fad684a1d8dc4b1749716051df9d5d9c7948b4e8
yash-a11y <73629345+yash-a11y@users.noreply.github.com>
2022-08-05 23:56:37 +0530

    adding database

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose = require("mongoose");
+
+
+
 
 const homeStartingContent = "Welcome write your daily moments";
 const aboutContent = "@yas web webdeveloper";
@@ -15,15 +19,43 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+//mongodb connection using mongoose
 
+  mongoose.connect("mongodb://localhost/dailygernalDB", {useNewUrlParser : true});
+
+  const dataschema = {
+    title : String,
+    content : String
+  };
+
+  const datamodel = mongoose.model("Note",dataschema);
+
+
+//END
 let posts = [];
 
 
 app.get("/", function(req, res){
-  res.render("home", {
-    startingContent: homeStartingContent,
-    posts: posts
-    });
+  
+  datamodel.find({},(err,founddata) =>
+  {
+    if(founddata == null)
+    {
+      datamodel.insertOne("Your Personal dairy",(err) =>
+      {
+        if(err) console.log("data is empty");
+        else console.log("data is inserted successfully");
+      });
+
+      res.redirect("/");
+
+    }
+    else{
+    res.render("home", {
+      startingContent: homeStartingContent,
+      posts: founddata
+      });}
+  });
 });
 
 app.get("/about", function(req, res){
@@ -39,32 +71,44 @@ app.get("/compose", function(req, res){
 });
 
 app.post("/compose", function(req, res){
-  const post = {
-    title: req.body.postTitle,
-    content: req.body.postBody
-  };
+  
+  const titlei = req.body.postTitle;
+  const des = req.body.postBody;
 
-  posts.push(post);
+  const item = new datamodel(
+    {
+      title : titlei,
+      content : des
+    }
+  );
 
+  item.save();  
   res.redirect("/");
-
 });
 
 app.get("/posts/:postName", function(req, res){
   const requestedTitle = _.lowerCase(req.params.postName);
 
-  posts.forEach(function(post){
-    const storedTitle = _.lowerCase(post.title);
 
-    if (storedTitle === requestedTitle) {
-      res.render("post", {
-        title: post.title,
-        content: post.content
-      });
-    }
+
+  datamodel.findOne({title : requestedTitle},(err,founddata) =>
+  {
+      if(err) console.log("data is empty");
+      else
+      {
+        
+        res.render("post",
+        {
+          title : founddata.title,
+          content : founddata.content == null ? "opps note is empty" : founddata.content
+        });
+      }
   });
+  
+
 
 });
+
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
